3af82cf3d650f2a95879b1eda2f31f0d6c395e6a
Subhadip Ray <roysubhadip0807@gmail.com>
2024-05-30 09:11:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import AgeCalculator from './Components/AgeCalculator';
+function App() {
+    return (
+        <div clsssName='App'>
+            <AgeCalculator />
+        </div>
+    );
+}
+export default App;
